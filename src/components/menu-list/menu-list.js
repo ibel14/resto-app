d82c8b85a14fea114cc7201cd7a3e4094e968192
ponaehali <iambelousov@gmail.com>
2021-02-10 13:37:27 +0200

    added requests

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -2,21 +2,26 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import {connect} from 'react-redux';
 import WithRestoService from '../hoc';
-import {menuLoaded} from '../../actions';
+import {menuLoaded, menuRequested} from '../../actions';
+import Spinner from '../spinner';
 
 import './menu-list.scss';
 
 class MenuList extends Component {
 
     componentDidMount() {
+        this.props.menuRequested();
         const {RestoService} = this.props;
         RestoService.getMenuItems()
         .then(res => this.props.menuLoaded(res));
     }
 
     render() {
-        const {menuItems} = this.props;
+        const {menuItems, loading} = this.props;
 
+        if (loading) {
+            return <Spinner/>
+        }
         return (
             <ul className="menu__list">
                 {
@@ -31,12 +36,14 @@ class MenuList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        menuItems: state.menu
+        menuItems: state.menu,
+        loading: state.loading
     }
 };
 
 const mapDispatchToProps = {
-    menuLoaded
+    menuLoaded,
+    menuRequested
 };
 
 
